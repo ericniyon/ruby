0fc3b4839e1075ba9c14d388bdd96930da5fa660
IREN <erecniyo2@outlook.com>
2019-01-10 11:59:01 +0430

    ruby page

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'news/index'
+
+  get 'news/show'
+
+  get 'home/index'
+
+  get 'home/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
